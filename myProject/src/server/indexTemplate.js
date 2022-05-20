dc0039e8c59828c9147077af4c48d3f3b2acb691
Джахпарова Тамара <ragimova-tamara@mail.ru>
2022-05-20 17:18:34 +0300

    fix

--- myProject/src/server/indexTemplate.js
+++ myProject/src/server/indexTemplate.js
@@ -4,7 +4,6 @@ export const indexTemplate = (content, token) => `
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta http-equiv="Content-Security-Policy" content="default-src 'self' https: ; object-src 'none'" />
 
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
